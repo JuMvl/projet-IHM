7809b41fa4e7b4fd3664183cd8d882b6132963df
LouiseBarthon <117666030+LouiseBarthon@users.noreply.github.com>
2023-04-30 14:13:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,43 +5,50 @@ import javafx.scene.*;
 import javafx.fxml.*;
 
 /**
- * Décrivez votre classe Main ici.
+ * Main est la classe principale qui lance l'application
  *
- * @author (votre nom)
- * @version (un numéro de version ou une date)
+ * @author 
+ * @version 30/04/23
  */
 public class Main extends Application {
     private Stage primaryStage; 
     
+    
+    /**
+    *initialissation de l'application
+    */
     public static void main(String[] args) {
          Application.launch(args);
     }
     
+    /**
+    *Creation et affichage de la fenetre principale page_accueil.fxml
+    */
     @Override
     public void start(Stage primaryStage) throws Exception
     {
         this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("page_accueil.fxml");
+        this.primaryStage.setTitle("page_accueil.fxml");//definit le titre de la fenetre principale
         
             try
         {
-            Parent root = FXMLLoader.load(getClass().getResource("page_accueil.fxml"));
-            Scene scene = new Scene (root);// JavaFX must have a Scene (window content) inside a Stage (window)
+            Parent root = FXMLLoader.load(getClass().getResource("page_accueil.fxml"));//charge le fichier FXML page_accueil.fxml
+            Scene scene = new Scene (root);//creation d'une nouvelle scène
             // Création de la scène (avec des dimensions correspondant à une résolution HD), composé uniquement d'une étiquette affichant du texte
             primaryStage.setScene(scene);
-            primaryStage.show();
+            primaryStage.show();//affichage de la fenetre
             
         }
-        catch (IOException ioe)
+  
+        catch (IOException ioe)//si un problème survient pendant le chargement du fichier une erreur IOExceptioon apparait
         {
             ioe.printStackTrace();
-        }// Building the FXML hierarchy
+        }
 
     }
     
     /**
-     * Returns the main stage.
-     * @return
+     * GetPrimaryStage renvoie l'object Stage utilisé pour afficher la fenetre principale de l'application
      */
     public Stage getPrimaryStage() {
         return primaryStage;
